0595d7499648237027c121cfd38cb70ce196dd50
Peter Jiang <w224jian@uwaterloo.ca>
2021-12-19 13:15:14 -0500

    actix version

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,9 @@
-#[macro_use] extern crate rocket;
-use rocket::response::content;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Serialize, Deserialize};
 use serde_json::{Result, Value};
 
-#[get("/versions/<rtype>")]
-fn versions(rtype: String) -> content::Json<String> {
+#[get("/versions/{rtype}")]
+async fn versions(web::Path(rtype): web::Path<String>) -> Result<String> {
     let response: Response = serde_json::from_str(minreq::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
     .send().unwrap().as_str().unwrap()).unwrap();
     let mut r = Vec::new();
@@ -13,20 +12,20 @@ fn versions(rtype: String) -> content::Json<String> {
             r.push(version.id);
         }
     }
-    content::Json(serde_json::to_string(&r).unwrap())
+    Ok(serde_json::to_string(&r).unwrap())
 }
 
-#[get("/server/<version>")]
-fn server(version: String) -> content::Json<String> {
+#[get("/server/{version}")]
+async fn server(web::Path(version) : web::Path<String>) -> Result<String> {
     let response: Response = serde_json::from_str(minreq::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
     .send().unwrap().as_str().unwrap()).unwrap();
     for version_indiv in response.versions {
         if version_indiv.id == version {
            let response : Value = serde_json::from_str(minreq::get(version_indiv.url).send().unwrap().as_str().unwrap()).unwrap();
-           return content::Json(response["downloads"]["server"]["url"].to_string());
+           return Ok(response["downloads"]["server"]["url"].to_string());
         }
     }
-    content::Json("error".to_string())
+    Ok("error".to_string())
     
 }
 
@@ -45,10 +44,14 @@ struct Response {
     versions: Vec<Version>,
 }
 
-#[launch]
-fn rocket() -> _ {
-
-    // let response = minreq::get("http://launchermeta.mojang.com/mc/game/version_manifest.json").send().unwrap();
-    // println!("{}", response.as_str().unwrap());
-    rocket::build().mount("/", routes![versions, server])
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(versions)
+            .service(server)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
